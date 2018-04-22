84a8c2af68f5c2cda4306dd045e9ea43eefa8c6e
Peter Toth <tothpeter08@gmail.com>
2018-04-22 17:19:10 +0200

    Fetch the right content for the index.html

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,21 @@
 class PagesController < ApplicationController
   def index
-    sql = "SELECT * FROM client_bootstrap ORDER BY id DESC LIMIT 1"
+    sql = generate_sql
     records_array = ActiveRecord::Base.connection.execute(sql).to_a
 
-    html_content = records_array[0]['value']
+    html_content = records_array.present? ? records_array[0]['value'] : 'No html content'
 
     render html: html_content.html_safe
+  end
+
+  private
+
+  def generate_sql
+    if params[:revision]
+      revision = ActiveRecord::Base.connection.quote(params[:revision])
+      "SELECT value FROM client_bootstrap WHERE key = #{revision}"
+    else
+      "SELECT value FROM client_bootstrap WHERE is_active = true ORDER BY id DESC LIMIT 1"
+    end
   end
 end
