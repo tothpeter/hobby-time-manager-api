da4e893c8bd9decd6fda9f8d301b2e254f0c4c57
Peter Toth <tothpeter08@gmail.com>
2018-04-22 16:41:02 +0200

    Update seed to be able to use it in production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-require 'database_cleaner'
 require_relative 'seed_helper'
 
-DatabaseCleaner.clean_with(:truncation)
+tables_to_truncate = %w(tasks authentication_tokens users)
+
+ActiveRecord::Base.connection.execute("TRUNCATE #{tables_to_truncate.join(',')} RESTART IDENTITY")
 
 SeedHelper::Users.create
 SeedHelper::Users.sign_in_my_user
